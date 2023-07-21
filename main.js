fcab2ac56012221f34d4053089532d07e6c8252a
Jeremias Storm <jerestorm@gmail.com>
2023-07-21 19:37:42 -0300

    se agregaron los jugadores y se tomaron desde js

--- main.js
+++ main.js
@@ -1,2 +1,17 @@
-"use strict"
-alert("hola mundo")
+"use strict";
+
+
+let player1 = document.querySelector("#jugador1");
+console.log(player1)
+
+let player2 = document.getElementById("jugador2");
+console.log(player2);
+
+let btnMover1 = document.querySelector("#moverJugador1");
+
+btnMover1.addEventListener("click", moverPlayer1);
+
+function moverPlayer1() {
+    let data = player1.getBoundingClientRect();
+    player1.style.top = data.top + 15;
+}
